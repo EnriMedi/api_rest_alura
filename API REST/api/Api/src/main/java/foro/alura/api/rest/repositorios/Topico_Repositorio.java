279d5db943b42522c59f51f0ac3783256b6233cf
Enrique Medina <144550644+EnriMedi@users.noreply.github.com>
2025-01-13 09:27:18 -0600

    Delete API REST/api/Api directory

--- API REST/api/Api/src/main/java/foro/alura/api/rest/repositorios/Topico_Repositorio.java
+++
@@ -1,8 +0,0 @@
-package foro.alura.api.rest.repositorios;
-import foro.alura.api.rest.modelos.Topicos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Topico_Repositorio extends JpaRepository<Topicos, Integer> {
-}
